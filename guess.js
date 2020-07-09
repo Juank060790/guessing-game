3195ac0c383cd307cf232220354e304795578777
Juank060790 <juank060790@gmail.com>
2020-07-09 11:43:08 +0700

    final guessing game

--- guess.js
+++ guess.js
@@ -3,9 +3,10 @@
 const guess = document.getElementById("guessingArea").value;
 
 
-// let time = 30 // time start from 0
-// let myTime; // timer will be assign to this variable
 
+// let myTime; // timer will be assign to this variable
+let time = 30 // time start from 0
+let myTime; // timer will be assign to this variable
 // array
 let history = []
 let results = []
@@ -20,20 +21,38 @@ console.log(randomNumber)
 
 // guesses remaining
 document.getElementById("guess-remaining").innerHTML = `${5}`
-timecounting() 
+timecounting()
 addingResults()
 
-function addingResults(){
+function addingResults() {
     let resultNumber = document.getElementById("guessingArea").value
     results.push(resultNumber)
     document.getElementById("results").innerHTML.value = `results: ${results}`
-    
+
 }
 
 function guessNumber() {
-    
-    document.getElementById("resultArea").innerHTML = `Too low`
     let userNumber = document.getElementById("guessingArea").value
+
+    if (history.includes(userNumber)){
+        document.getElementById("resultArea").innerHTML = 'You have that number already'
+        document.getElementById("guessingArea").value = ''
+        return;
+    }
+
+    if (userNumber <= 0 || userNumber > 100){
+        document.getElementById("resultArea").innerHTML = 'The number must be between 1 and 100'
+        document.getElementById("guessingArea").value = ''
+        return;
+    }
+
+    // for (i = 0; i < history.length; i++) {
+    //     if (history[i] == userNumber) {
+    //         document.getElementById("resultArea").innerHTML = 'You have that number already'
+    //         return;
+    //     }
+    // }
+
     console.log(userNumber, "random", randomNumber)
     if (userNumber > randomNumber) {
         document.getElementById("resultArea").innerHTML = `Too High`
@@ -42,19 +61,20 @@ function guessNumber() {
     } else if (userNumber == randomNumber) {
         document.getElementById("resultArea").innerHTML = `You are safe you Don't drink!!`
     }
-    
+
     history.push(userNumber)
     document.getElementById("historyArea").innerHTML = `history: ${history}`
     document.getElementById("guessingArea").value = ''
     document.getElementById("guess-remaining").innerHTML = amountGuesssing = amountGuesssing - 1
-    if (amountGuesssing === -1) {
+    if (amountGuesssing === 0) {
         resetGame()
-        alert("Time to Drink!!") 
+        clearInterval(myTime)
+        alert("Time to Drink!!")
     }
 }
 
 function resetGame() {
-    
+
     amountGuesssing = 5
     document.getElementById("guess-remaining").innerHTML = `${amountGuesssing}`
     history = []
@@ -62,14 +82,16 @@ function resetGame() {
     document.getElementById("historyArea").innerHTML = `history: ${history}`
     console.log(randomNumber)
     document.getElementById("guessingArea").innerHTML = ""
-    
+    let time = 30
+    document.getElementById('timecount').innerHTML = `${time}`
+
 }
 
 
 
 function timecounting() {
-    let time = 30 // time start from 0
-    let myTime; // timer will be assign to this variable
+    
+    
     myTime = setInterval(() => {
         time -= 1
         document.getElementById('timecount').innerHTML = time
@@ -77,9 +99,9 @@ function timecounting() {
             setTimeout(() => alert("You LooOOOse!!"), 100);
             clearInterval(myTime)
         }
-        
+
     }, 1000)
-    
+
 }
 
 
---
+++ test.js
@@ -0,0 +1,7 @@
+let history = [2, 3, 4]
+let userNumber = 2
+for (i = 0; i < history.length; i ++){
+    if (userNumber == history[i]) {
+        console.log("you have that number alredy")
+    }
+}
